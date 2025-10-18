02c9634db7315dd56dfefd4dc22bea6ce296ab8e
kircatoon <hihka302@gmail.com>
2025-10-18 16:13:57 +0300

    Update test_figures.cpp

--- laba3/tests/test_figures.cpp
+++ laba3/tests/test_figures.cpp
@@ -75,71 +75,8 @@ TEST(FigureTest, InputOutput) {
     EXPECT_TRUE(triangle.isValid());
     EXPECT_NEAR(static_cast<double>(triangle), 6.0, 1e-9);
 }
-// Добавляем в существующий файл тестов
 
-TEST(ComparisonTest, TriangleEquality) {
-    Triangle triangle1(Point(0, 0), Point(3, 0), Point(0, 4));
-    Triangle triangle2(Point(0, 0), Point(3, 0), Point(0, 4));
-    Triangle triangle3(Point(0, 0), Point(4, 0), Point(0, 3));
-    
-    EXPECT_TRUE(triangle1 == triangle2);
-    EXPECT_FALSE(triangle1 == triangle3);
-}
-
-TEST(ComparisonTest, SquareEquality) {
-    Square square1(Point(0, 0), 5.0);
-    Square square2(Point(0, 0), 5.0);
-    Square square3(Point(1, 1), 5.0);
-    
-    EXPECT_TRUE(square1 == square2);
-    EXPECT_FALSE(square1 == square3);
-}
-
-TEST(ComparisonTest, RectangleEquality) {
-    Rectangle rect1(Point(0, 0), 4.0, 6.0);
-    Rectangle rect2(Point(0, 0), 4.0, 6.0);
-    Rectangle rect3(Point(0, 0), 5.0, 6.0);
-    
-    EXPECT_TRUE(rect1 == rect2);
-    EXPECT_FALSE(rect1 == rect3);
-}
-
-TEST(ComparisonTest, DifferentTypesNotEqual) {
-    Triangle triangle(Point(0, 0), Point(3, 0), Point(0, 4));
-    Square square(Point(0, 0), 6.0); // Такая же площадь как у треугольника
-    
-    EXPECT_FALSE(triangle == square);
-    EXPECT_FALSE(square == triangle);
-}
-
-TEST(ArrayComparisonTest, ArrayEquality) {
-    Array array1;
-    array1.addFigure(std::make_shared<Triangle>(3.0));
-    array1.addFigure(std::make_shared<Square>(Point(0, 0), 2.0));
-    
-    Array array2;
-    array2.addFigure(std::make_shared<Triangle>(3.0));
-    array2.addFigure(std::make_shared<Square>(Point(0, 0), 2.0));
-    
-    Array array3;
-    array3.addFigure(std::make_shared<Triangle>(4.0)); // Другой треугольник
-    
-    EXPECT_TRUE(array1 == array2);
-    EXPECT_FALSE(array1 == array3);
-}
-
-TEST(ArrayComparisonTest, DifferentSizesNotEqual) {
-    Array array1;
-    array1.addFigure(std::make_shared<Triangle>(3.0));
-    
-    Array array2;
-    array2.addFigure(std::make_shared<Triangle>(3.0));
-    array2.addFigure(std::make_shared<Square>(Point(0, 0), 2.0));
-    
-    EXPECT_FALSE(array1 == array2);
-}
-// ДОБАВИТЬ В КОНЕЦ ФАЙЛА (перед main):
-
+// НОВЫЕ ТЕСТЫ ДЛЯ ОПЕРАТОРОВ СРАВНЕНИЯ
 TEST(ComparisonTest, TriangleEquality) {
     Triangle triangle1(Point(0, 0), Point(3, 0), Point(0, 4));
     Triangle triangle2(Point(0, 0), Point(3, 0), Point(0, 4));
